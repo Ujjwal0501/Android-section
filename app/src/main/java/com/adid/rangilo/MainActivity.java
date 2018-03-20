707bdb2d100fc520d393dacaa0c2e1185e2d84ca
Punit <punit18071999@gmail.com>
2018-03-20 18:45:15 +0530

    noChanges

--- app/src/main/java/com/adid/rangilo/MainActivity.java
+++ app/src/main/java/com/adid/rangilo/MainActivity.java
@@ -1,12 +1,21 @@
 package com.adid.rangilo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
@@ -18,40 +27,131 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     Button ch;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FusedLocationProviderClient mFusedLocationClient;
+    final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    TextView tvloc;
+    String str = "";
+    TextView tvBal, ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
 
+
+        tvloc = (TextView) findViewById(R.id.tvloc);
         ch = (Button) findViewById(R.id.chatbox);
         ch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create a new user with a first and last name
-                Map<String, Object> user = new HashMap<>();
-                user.put("first", "Ada");
-                user.put("last", "Lovelace");
-                user.put("born", 1815);
-
-                // Add a new document with a generated ID
-                db.collection("users/bad")
-                        .add(user)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d("err", "DocumentSnapshot added with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w("err", "Error adding document", e);
-                            }
-                        });
-
+                getLoc();
+                tvloc.setText(str);
             }
         });
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        updateContent();
+    }
+
+    protected void createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5500);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    // update content of main page
+    private static void updateContent() {
+        //TODO
 
     }
+
+    // get balance; manual update only; auto-update on create
+    private static void getBalance() {
+        // TODO
+    }
+
+    // get location update; returns coordinates
+    private void getLoc() {
+        // TODO
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+                // showExplanation("Permission Needed", "Rationale", android.Manifest.permission.ACCESS_FINE_LOCATION, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            }
+        } else {
+            // Permission has already been granted
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            str =  "Could not Retrieve location";
+                            if (location != null) {
+                                // Logic to handle location object
+                                str = ""+ (location.getLatitude()) + "," +
+                                        "  gh" + location.getLongitude();
+                            }
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    getLoc();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    // check current spot and spots in DB
+    // takes return value of getLoc, returns TRUE/FALSE
+    private static void checkSpot() {
+        // TODO
+    }
+
+    // explored spots; only called when return of checkSpot is true
+    private static void expStat() {
+        // TODO
+    }
+
 }
